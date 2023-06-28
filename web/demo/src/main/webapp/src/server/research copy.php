1b8e9909d9f6adc7371cbafcf57797d39ce91375
elevensy27 <92013378+elevensy27@users.noreply.github.com>
2023-06-29 01:23:16 +0800

    完善了检索功能

---
+++ web/demo/src/main/webapp/src/server/research copy.php
@@ -0,0 +1,57 @@
+<?php
+header("content-type:text/json;charset=utf-8");
+
+$filename = 'path1.txt';
+if (file_exists($filename)) {
+    $file = fopen($filename, 'r');
+    $pathArray = file($filename, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($pathArray as $key => $value) {
+        echo "{$key}: {$value}\n";
+    }
+    fclose($file);
+} else {
+    echo '文件不存在';}
+
+    
+
+    for($i=0;$i<count($pathArray);$i++){
+        $contain='';
+        $path=$pathArray[$i];
+        //读取内容
+        $contain = file_get_contents($path);
+      
+
+        //检索
+        $keys='a';
+        $keyArrray;//目录所有文件含有key的句子
+
+        $con = explode($keys,$contain);//判断内容是否含有key
+        $c=0;
+
+        if(count($con)>1){
+            $line = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);//获取line
+
+            $i1=0;
+            $keylineArrray=[];//当个文件中含有key的句子
+
+            for($j=0;$j<count($line);$j++){
+                $c=count(explode($keys,$line[$j]));
+                if($c>1){  
+                    $keylineArrray[$i1]["row"]=$j;
+                    $keylineArrray[$i1]["contain"]=$line[$j];
+                    $i1++;
+                }
+            }
+
+            $keyArrray[$i]["path"]=$pathArray[$i];
+            $keyArrray[$i]["json"]=$keylineArrray;
+            
+            
+        }
+    
+    }
+    echo json_encode($keyArrray,JSON_UNESCAPED_UNICODE);
+
+    
+
+?>
--- web/demo/src/main/webapp/src/server/research.php
+++ web/demo/src/main/webapp/src/server/research.php
@@ -1,58 +1,74 @@
 <?php
+require 'pdfRead.php';
+require 'wordRead.php';
+
 header("content-type:text/json;charset=utf-8");
 
-$filename = 'path.txt';
+$filename = 'path.txt';//测试
+//$filename = $_POST["fileName"];
 if (file_exists($filename)) {
     $file = fopen($filename, 'r');
     $pathArray = file($filename, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-    foreach ($pathArray as $key => $value) {
-        echo "{$key}: {$value}\n";
-    }
     fclose($file);
-} else {
-    echo '文件不存在';}
+    //输出路径
+    // foreach ($pathArray as $key => $value) {
+    //     echo "{$key}: {$value}\n";
+    // }   
+} else {echo '文件不存在';}
 
-    $i=0;$j=0;$fileNum=0;$row=0;
-    for($i=0;$i<count($pathArray);$i++){
-        $contain='';
-        //$path=$pathArray[$i];
-        $path=$pathArray[$i];
-        //读取内容
-        $contain = file_get_contents($path);
-        $line=[0];
-        if (file_exists($path)) {
-            $file = fopen($path, 'r');
-            fclose($file);
-        } else {
-            echo '文件不存在';}
-
-        //检索
-        $keys='a';
-        $keyArrray;
-        $con = explode($keys,$contain);$c=0;
-        //echo count($con);
-        if(count($con)>1){
-            $line = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-            $i1=0;
-            $keylineArrray=[];
-            for($j=0;$j<count($line);$j++){
-                //echo $pathArray[$i];
-                $c=count(explode($keys,$line[$j]));
-                if($c>1){  
-                    $keylineArrray[$i1]["row"]=$j;
-                    $keylineArrray[$i1]["contain"]=$line[$j];
-                    $i1++;
+//检索
+function search($a,$b){
+    $line=$a;
+    $keys=$b;
+    $contain=implode('', $line);
+    $con = explode($keys,$contain);
+    $keylineArrray=[];
+    $c=0;
+    //echo count($con);
+    if(count($con)>1){
+        $i1=0;
+        for($j=0;$j<count($line);$j++){
+            //echo $pathArray[$i];
+            $c=count(explode($keys,$line[$j]));
+            if($c>1){  
+                $keylineArrray[$i1]["row"]=$j;
+                $keylineArrray[$i1]["contain"]=$line[$j];
+                $i1++;
                 }
-            }
-            $keyArrray[$i]["path"]=$pathArray[$i];
-            $keyArrray[$i]["json"]=$keylineArrray;
-            
-            
+            }        
         }
-    
-    }
-    echo json_encode($keyArrray,JSON_UNESCAPED_UNICODE);
+        return $keylineArrray;
+}
 
+
+    $i=0;
+    for($i=0;$i<count($pathArray);$i++){
+        //$contain='';
+
+        $keys='a';//测试
+        //从前端获取关键字
+        //$keys=$_POST["Keys"];
+        $keyArrray=[];
+
+        //判断是word还是pdf
+        $path=$pathArray[$i];
+        if(count(explode('pdf',$path))>1){
+            $pdfline=pdfread($path);
+            $keylineArrray=search($pdfline,$keys);
+            $keyArrray[$i]["path"]=$pathArray[$i];
+            $keyArrray[$i]["json"]=$keylineArrray;}
+        // else if(count(explode('doc',$path))>1 or count(explode('docx',$path))>1){
+        //     $wordline=wordread($path);
+        //     $keylineArrray=search($wordline,$keys);
+        //     $keyArrray[$i]["path"]=$pathArray[$i];
+        //     $keyArrray[$i]["json"]=$keylineArrray; 
+        // }
+          
+        
+
+    //利用json_encode()方法将数组转换为json，并利用echo方法输出json
+    echo json_encode($keyArrray,JSON_UNESCAPED_UNICODE);
+    }
     
 
 ?>
