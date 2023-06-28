99d246fe5047d27c193d530f831653d38c6ce2d3
cjl1828103646 <86905574+cjl1828103646@users.noreply.github.com>
2023-06-28 15:10:13 +0800

    Add files via upload

---
+++ web/demo/src/main/webapp/src/server/readfile.php
@@ -0,0 +1,51 @@
+<?php
+function searchFile($path, &$file_array, &$folder_array, &$all_array){
+    if(is_dir($path)){  //检查文件目录是否存在
+        $H = @ opendir($path);
+        while(false !== ($_file=readdir($H))){
+            //检索目录
+            if(is_dir($path."/".$_file) && !in_array($_file, array(".", "..", "Thumbs.db")))
+            {
+                array_push($folder_array, $path."/".$_file);
+                array_push($all_array, $path."/".$_file);
+                searchFile($path."/".$_file, $file_array, $folder_array, $all_array);
+            //检索文件
+            }
+            elseif(file_exists($path."/".$_file) && !in_array($_file, array(".", "..", "Thumbs.db")))
+            {
+                array_push($file_array,$path."/".$_file);
+                array_push($all_array, $path."/".$_file);
+            }
+        }
+        closedir($H);
+    }elseif(file_exists($path)){
+        array_push($file_array, $path);
+        array_push($all_array, $path);
+    }
+}
+ 
+$file_array=array(); //存放文件名数组  
+$folder_array=array(); //存放目录名数组  
+$all_array=array(); //存放全部路径的数组 
+
+$docsearch=array();
+$docxsearch=array();
+$pdfsearch=array();
+
+$floderpath = "C:\\Users\\cjl\\Desktop\\2020212205134_陈佳璐";
+
+searchFile($floderpath, $file_array, $folder_array, $all_array);
+foreach ($all_array as $value) {
+    //echo $value."\n";
+    if(substr(strrchr($value,'.'),1)=="doc"){
+        array_push($docsearch,$value);
+    }
+    else if(substr(strrchr($value,'.'),1)=="docx"){
+        array_push($docxsearch,$value);
+    }
+    else if(substr(strrchr($value,'.'),1)=="pdf"){
+        array_push($$pdfsearch,$value);
+    }
+}
+echo json_encode($docsearch,JSON_UNESCAPED_UNICODE);
+?>
