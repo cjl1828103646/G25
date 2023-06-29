2020e9bf15b7c84b463708efefeefa403990f999
cjl1828103646 <86905574+cjl1828103646@users.noreply.github.com>
2023-06-30 02:47:26 +0800

    更新了后端目录地址检索

--- web/demo/src/main/webapp/src/page/index.js
+++ web/demo/src/main/webapp/src/page/index.js
@@ -109,3 +109,106 @@ console.log(res);
 }
 }
 
+function sendData(path,callback){
+  var arr ={
+    "path":path
+    };
+  var json =JSON.stringify(arr);//使用JSON将对象转换成JSON格式数据
+  var xhr = new XMLHttpRequest;
+
+  xhr.open('post', 'server/readfile1.php');
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhr.send("data=" + json);//  Content-Type设置成application/x-www-form-urlencoded 的情况下，请求主体可以用key1=value1&key2=value2的形式发送数据
+  xhr.onreadystatechange = function() {
+       if(xhr.readyState == 4 && (xhr.status == 200 || xhr.status ==304)){  //响应完成并且响应码为200或304
+       //console.log(xhr.responseText);
+      //  data = xhr.responseText;
+      //  json=JSON.parse(data);
+      callback(JSON.parse(xhr.responseText));
+       }
+  };
+
+}
+
+function go(){
+  let dx = document.getElementsByName("menu");
+  var responseArray = [];
+  //var path = [];
+  for(var i=0;i<dx.length;i++){
+    if(dx[i].checked){
+      //alert(dx[i].value);
+      var t = document.getElementById("search");
+      t.value=dx[i].value;
+
+      sendData(dx[i].value, function(response) {
+        console.log(response);
+        responseArray = responseArray.concat(response);
+        var menuread = document.getElementById("menuread");
+        menuread.innerHTML="";
+        for(var i=0;i<responseArray.length;i++){
+          //back();
+          creakradiobox(responseArray[i]);
+        }
+        console.log(responseArray);
+        if(responseArray==[]){
+          alert("没有更多啦！");
+        } 
+        });
+      break;
+    }
+  }
+
+}
+
+function creakradiobox(name1){
+  // var container = document.getElementById("menuread");
+  var checkbox = document.createElement('input');
+
+    checkbox.type = 'radio';
+    checkbox.value = name1;
+  checkbox.name = "menu";
+  
+    var checkboxLabel = document.createElement('label');
+    checkboxLabel.setAttribute('for', name1);
+    checkboxLabel.textContent = name1;
+  
+    //var container = document.createElement('div');
+    menuread.appendChild(checkbox);
+    menuread.appendChild(checkboxLabel);
+
+  var lineBreak = document.createElement('br');
+  menuread.appendChild(lineBreak);
+
+    return menuread;
+}
+
+function back(){
+  var t = document.getElementById("search");
+  var tx = t.value;
+  var responseArray = [];
+  if(tx=="请选择目录"){
+    alert("已经到头啦")
+  }  
+  else if(tx=="C:"){
+    location.reload(); 
+  }
+  else{
+    var tx1 = tx.split('\\');
+    tx1.pop();
+    tx = tx1.join("\\");
+    t.value=tx;
+  sendData(tx, function(response) {
+    console.log(response);
+    responseArray = responseArray.concat(response); 
+    var menuread = document.getElementById("menuread");
+    menuread.innerHTML="";
+    for(var i=0;i<responseArray.length;i++){
+          //back();
+
+      creakradiobox(responseArray[i]);
+    }
+    console.log(responseArray);
+    });
+      
+    }
+}
