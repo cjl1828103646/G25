5cfde89a3c69501bbc16cf37c4a82f78bfcf5fb8
elevensy27 <92013378+elevensy27@users.noreply.github.com>
2023-06-29 19:38:35 +0800

    完善了检索功能

--- web/demo/src/main/webapp/src/server/research.php
+++ web/demo/src/main/webapp/src/server/research.php
@@ -1,20 +1,23 @@
 <?php
-require 'pdfRead.php';
-require 'wordRead.php';
-
 header("content-type:text/json;charset=utf-8");
+require 'pdfRead.php';
+require 'readfile.php';
+//require 'wordRead.php';
 
-$filename = 'path.txt';//测试
-//$filename = $_POST["fileName"];
-if (file_exists($filename)) {
-    $file = fopen($filename, 'r');
-    $pathArray = file($filename, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-    fclose($file);
-    //输出路径
-    // foreach ($pathArray as $key => $value) {
-    //     echo "{$key}: {$value}\n";
-    // }   
-} else {echo '文件不存在';}
+if (isset($_POST["path"])) {
+    $floderpath = $_POST["path"];
+    $pathArray=pathresult($floderpath);
+}else{$pathArray=["失败啦！","hhh"];}
+// $floderpath = "C:\\Users\\asus\\Desktop\\test";//测试
+// $pathArray=pathresult($floderpath);
+
+//$keys='a';//测试
+//从前端获取关键字
+if (isset($_POST["Keys"])) {
+    $keys=$_POST["Keys"];
+}else{$keys="NO KEYS";}
+
+$keyArrray = array();
 
 //检索
 function search($a,$b){
@@ -41,13 +44,9 @@ function search($a,$b){
 }
 
 
-    $i=0;
+    $i=0;$j=0;
+    if(!empty($pathArray)){
     for($i=0;$i<count($pathArray);$i++){
-        //$contain='';
-
-        $keys='a';//测试
-        //从前端获取关键字
-        //$keys=$_POST["Keys"];
         $keyArrray=[];
 
         //判断是word还是pdf
@@ -55,20 +54,24 @@ function search($a,$b){
         if(count(explode('pdf',$path))>1){
             $pdfline=pdfread($path);
             $keylineArrray=search($pdfline,$keys);
-            $keyArrray[$i]["path"]=$pathArray[$i];
-            $keyArrray[$i]["json"]=$keylineArrray;}
+            $keyArrray[$j]["path"]=$pathArray[$i];
+            $keyArrray[$j]["json"]=$keylineArrray;}
         // else if(count(explode('doc',$path))>1 or count(explode('docx',$path))>1){
         //     $wordline=wordread($path);
         //     $keylineArrray=search($wordline,$keys);
-        //     $keyArrray[$i]["path"]=$pathArray[$i];
-        //     $keyArrray[$i]["json"]=$keylineArrray; 
+        //     $keyArrray[$j]["path"]=$pathArray[$i];
+        //     $keyArrray[$j]["json"]=$keylineArrray; 
         // }
           
-        
-
-    //利用json_encode()方法将数组转换为json，并利用echo方法输出json
-    echo json_encode($keyArrray,JSON_UNESCAPED_UNICODE);
+    
     }
+}
+
+       //利用json_encode()方法将数组转换为json，并利用echo方法输出json
+       if(empty($keyArrray)){
+        $keyArrray="NULL";
+       }
+       echo json_encode($keyArrray,JSON_UNESCAPED_UNICODE);
     
 
 ?>
